d72efca9a18c78926d0a96bbc900f41bb779474a
Shailen Narainsamy <Shailen.Narainsamy@derivco.co.za>
2020-05-09 18:05:24 +0200

    Adding Model Instantiation

---
+++ server/RemoteGamingSytem.Sample.Api/Controllers/RGSController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RemoteGamingSytem.Sample.Api.Models;
+using RemoteGamingSytem.Sample.Api.ViewModel;
+using Jil;
+
+
+namespace RemoteGamingSytem.Sample.Api.Controllers
+{
+    [Microsoft.AspNetCore.Mvc.Route("api/[controller]")]
+    [ApiController]
+    public class RGSController : ControllerBase
+    {
+        // GET: api/RGS
+        [Microsoft.AspNetCore.Mvc.HttpGet]
+        public Methods  GetMethodDetails()
+        {
+            return new Methods()
+            {
+              StartGameSessionDetails = new StartGameSession(),
+              VerifyPlayerSessionDetails = new VerifyPlayerSession()
+            };
+        }
+
+        // GET: api/RGS/5
+        [Microsoft.AspNetCore.Mvc.HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/RGS
+        [Microsoft.AspNetCore.Mvc.HttpPost]
+        public VerifyPlayerSession VerifyPlayerSessionRequest([Microsoft.AspNetCore.Mvc.FromBody] string request)
+        {
+          var p = new Processing();
+          return p.InitVerifyPlayerSessionSession(request);
+        }
+
+        // PUT: api/RGS/5
+        [Microsoft.AspNetCore.Mvc.HttpPut("{id}")]
+        public void Put(int id, [Microsoft.AspNetCore.Mvc.FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [Microsoft.AspNetCore.Mvc.HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ server/RemoteGamingSytem.Sample.Api/Models/StartGameSession.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using RemoteGamingSytem.Sample.Api.AppConstants;
+
+namespace RemoteGamingSytem.Sample.Api.Models
+{
+  public class StartGameSession
+  {
+
+    [Required]
+    [StringLength(ApplicationConstants.BrandId)]
+    public string BrandId { get; set; }
+    
+    [Required]
+    [StringLength(ApplicationConstants.SkinId)]
+    public string SkinId { get; set; }
+    
+    [StringLength(ApplicationConstants.UniqueId)]
+    public string GameId { get; set; }
+
+    [Required]
+    public string ChannelType { get; set; }
+
+    [Required]
+    public string PresentType { get; set; }
+
+    [Required]
+    [StringLength(ApplicationConstants.SecureToken)]
+    public string SecureToken { get; set; }
+
+    [Required]
+    [StringLength(ApplicationConstants.PlayerId)]
+    public string PlayerId { get; set; }
+
+    [Required]
+    [StringLength(ApplicationConstants.AccountId)]
+    public string AccountId { get; set; }
+
+    [Required]
+    [StringLength(ApplicationConstants.LocaleCode)]
+    public string LocaleCode { get; set; }
+
+    [Required]
+    [StringLength(ApplicationConstants.CurrencyCode)]
+    public string CurrencyCode { get; set; }
+
+    [Required]
+    [StringLength(ApplicationConstants.GameSessionId)]
+    public string GameSessionId { get; set; }
+
+    [Required]
+    public string GameType { get; set; }
+
+    [Required]
+    [StringLength(ApplicationConstants.MfgCode)]
+    public string MfgCode { get; set; }
+
+    [Required]
+    [StringLength(ApplicationConstants.UniqueId)]
+    public string ThemeId { get; set; }
+
+    [Required]
+    [StringLength(ApplicationConstants.UniqueId)]
+    public string PaytableId { get; set; }
+
+    [StringLength(ApplicationConstants.ReleaseNumber)]
+    public string ReleaseNum { get; set; }
+
+    [Required]
+    [StringLength(ApplicationConstants.BetConfigId)]
+    public string BetConfigId { get; set; }
+
+    [Required]
+    [StringLength(ApplicationConstants.JurisdictionCode)]
+    public string JurisdictionCode { get; set; }
+  }
+}
+
