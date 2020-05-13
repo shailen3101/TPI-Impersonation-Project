5cec5234a395af52fe9d9c850e47a1fda4328d52
Thabo-Nkabinde <64193370+Thabo-Nkabinde@users.noreply.github.com>
2020-05-13 09:18:29 +0200

    Added Axios config to work with vue (#15)
    Co-authored-by: Thabo <thabojoy-gift.nkabinde@derivco.co.za>

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import Vuetify from 'vuetify/lib'
 
-Vue.use(Vuetify)
+Vue.use(Vuetify,VueAxios,axios)
 
 export default new Vuetify({
 })
