4d02e0ebc0486c2dcb919a95012228625e774043
Shane <shane.linden@derivco.co.za>
2020-05-15 12:26:23 +0200

    Removing old client

--- client/src/plugins/vuetify.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import axios from 'axios'
-import VueAxios from 'vue-axios'
-import Vuetify from 'vuetify/lib'
-
-Vue.use(Vuetify,VueAxios,axios)
-
-export default new Vuetify({
-})
