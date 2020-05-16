2d80edbb5cd65fca020daa62aed5131917025faf
Shane <shane.linden@derivco.co.za>
2020-05-16 11:06:40 +0200

    Adding the header and selection on the header

---
+++ client/src/util/data.js
@@ -0,0 +1,14 @@
+export const commandNames = [
+	'TPI_verifyPlayerSession',
+	'TPI_playerSessionAck',
+	'TPI_startGameSession',
+	'TPI_gameSessionAck',
+	'TPI_startGameCycle',
+	'TPI_startGameCycleAck',
+	'TPI_moneyTransactions',
+	'TPI_moneyTransactionsAck',
+	'TPI_endGameCycle',
+	'TPI_endGameCycleAck',
+	'TPI_endGameSession',
+	'TPI_gameSessionAck',
+]
