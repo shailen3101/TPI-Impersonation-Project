87f97051cae452a801169ce7f2be96a8c3d33fe4
Shane Linden <Shane.Linden@derivco.co.za>
2020-05-17 10:06:39 +0200

    Adding info to verify player session

---
+++ client/src/components/MethodContent/Request.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-row align="center" justify="center">
+    <v-col cols="12">
+      <v-card>
+        <v-card-title class="green--text">TPI_VerifyPlayerSession Request</v-card-title>
+        <v-card-subtitle class="black--text">Enter the JSON and hit send to get a Response</v-card-subtitle>
+        <v-col>
+          <app-form></app-form>
+        </v-col>
+        <v-col align="right">
+          <v-btn color="blue">Send Request</v-btn>
+        </v-col>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import AppForm from "./Form.vue";
+
+export default {
+  props: {
+    isSelected: Boolean
+  },
+  components: {
+    AppForm
+  }
+};
+</script>
--- client/src/views/Commands/Tpi_verifyPlayerSession.vue
+++ client/src/views/Commands/Tpi_verifyPlayerSession.vue
@@ -1,13 +1,27 @@
 <template>
-	<div>
-		VerifyPlayerSession
-	</div>
+  <v-container fluid>
+    <app-tab @changedTab="selectedTab"></app-tab>
+    <component :is="selectedComponent"></component>
+  </v-container>
 </template>
 
 <script>
-	export default {
-		created() {
-			console.log('Created')
-		},
-	}
+import AppTab from "../../components/MethodContent/Tab.vue";
+import AppRequest from "../../components/MethodContent/Request.vue";
+
+export default {
+  data: () => ({
+    selectedComponent: "app-request"
+  }),
+  components: {
+    AppTab,
+    "app-request": AppRequest
+  },
+  methods: {
+    selectedTab(tab) {
+      if (tab === "request") this.selectedComponent = "app-request";
+      else this.selectedComponent = "app-details";
+    }
+  }
+};
 </script>
