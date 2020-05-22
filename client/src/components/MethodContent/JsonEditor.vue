2b935c54886a18f0b3639e25aa4f90cd24b78770
Shane <shane.linden@derivco.co.za>
2020-05-22 12:00:59 +0200

    Adding Vuex and central state

---
+++ client/src/components/MethodContent/JsonEditor.vue
@@ -0,0 +1,33 @@
+<template>
+	<v-jsoneditor
+		v-model="json"
+		:options="options"
+		:plus="false"
+		height="400px"
+		@error="onError"
+	>
+	</v-jsoneditor>
+</template>
+
+<script>
+	import VJsoneditor from 'v-jsoneditor/src/index'
+
+	export default {
+		name: 'app',
+		components: {
+			VJsoneditor,
+		},
+		data() {
+			return {
+				json: {
+					hello: 'vue',
+				},
+			}
+		},
+		methods: {
+			onError() {
+				console.log('error')
+			},
+		},
+	}
+</script>
